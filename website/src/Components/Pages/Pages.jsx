6efa0d28be0472c010a9633ece3d2b3adc3e4be9
Pradeep-2564 <ppradeep4525@gmail.com>
2025-03-01 14:04:15 +0530

    Adding navbar and footer

---
+++ website/src/Components/Pages/Pages.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../Navbar/NavBar'
+import Footer from '../Footer/Footer'
+
+const Pages = () => {
+  return (
+    <div>
+      <NavBar/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Pages
